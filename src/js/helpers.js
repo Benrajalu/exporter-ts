946734e702aeb9cb8d1de99dfdddb32443789029
Fabien Rassinier <frassinier@talend.com>
2021-11-26 13:58:00 +0100

    feat: khebab case instead of camel case

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -1,13 +1,9 @@
-/**
- * Convert group name, token name and possible prefix into camelCased string, joining everything together
- */
-Pulsar.registerFunction(
-  "readableVariableName",
-  function (token, tokenGroup, prefix) {
+function getReadableVariableNameByCaseStyle(caseStyle) {
+  return function getReadableVariableName(token, tokenGroup, prefix) {
     // Create array with all path segments and token name at the end
     const segments = [...tokenGroup.path];
     if (!tokenGroup.isRoot) {
-      segments.push(tokenGroup.name)
+      segments.push(tokenGroup.name);
     }
     segments.push(token.name);
 
@@ -19,41 +15,65 @@ Pulsar.registerFunction(
     let sentence = segments.join(" ");
 
     // camelcase string from all segments
-     sentence = sentence
-      .toLowerCase()
-      .replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => chr.toUpperCase())
-    
-    // only allow letters, digits, underscore and hyphen
-    sentence = sentence.replace(/[^a-zA-Z0-9_-]/g, '_')
+    sentence = sentence.toLowerCase().replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => {
+      switch (caseStyle) {
+        case "khebab":
+          return "-" + chr;
+        case "camel":
+          return chr.toUpperCase();
+        default:
+          return chr;
+      }
+    });
 
-    // prepend underscore if it starts with digit 
+    // only allow letters, digits, underscore and hyphen
+    sentence = sentence.replace(/[^a-zA-Z0-9_-]/g, "_");
+
+    // prepend underscore if it starts with digit
     if (/^\d/.test(sentence)) {
-      sentence = '_' + sentence;
+      sentence = "_" + sentence;
     }
 
     return sentence;
-  }
+  };
+}
+
+/**
+ * Convert group name, token name and possible prefix into khebabCased string, joining everything together
+ */
+Pulsar.registerFunction(
+  "readableVariableKhebabName",
+  getReadableVariableNameByCaseStyle("khebab")
 );
 
+/**
+ * Convert group name, token name and possible prefix into camelCased string, joining everything together
+ */
+Pulsar.registerFunction(
+  "readableVariableCamelName",
+  getReadableVariableNameByCaseStyle("camel")
+);
 
-function findAliases(token, allTokens){
-  let aliases = allTokens.filter(t => t.value.referencedToken && t.value.referencedToken.id === token.id)
+function findAliases(token, allTokens) {
+  let aliases = allTokens.filter(
+    (t) => t.value.referencedToken && t.value.referencedToken.id === token.id
+  );
   for (const t of aliases) {
-    aliases = aliases.concat(findAliases(t, allTokens))
+    aliases = aliases.concat(findAliases(t, allTokens));
   }
   return aliases;
 }
 
-Pulsar.registerFunction("findAliases", findAliases)
+Pulsar.registerFunction("findAliases", findAliases);
 
-Pulsar.registerFunction("gradientAngle", function(from, to) {
-    var deltaY = (to.y - from.y);
-    var deltaX = (to.x - from.x);
-    var radians = Math.atan2(deltaY, deltaX); 
-    var result = radians * 180 / Math.PI; 
-    result = result + 90; 
-    return  ((result < 0) ? (360 + result) : result) % 360;
-})
+Pulsar.registerFunction("gradientAngle", function (from, to) {
+  var deltaY = to.y - from.y;
+  var deltaX = to.x - from.x;
+  var radians = Math.atan2(deltaY, deltaX);
+  var result = (radians * 180) / Math.PI;
+  result = result + 90;
+  return (result < 0 ? 360 + result : result) % 360;
+});
 
 /**
  * Behavior configuration of the exporter
@@ -71,26 +91,23 @@ Pulsar.registerPayload("behavior", {
 });
 
 Pulsar.registerFunction("rgbaToHsla", function (r, g, b, a = 1) {
-  var ratiodR = r/255;
-  var ratiodG = g/255;
-  var ratiodB = b/255;
+  var ratiodR = r / 255;
+  var ratiodG = g / 255;
+  var ratiodB = b / 255;
 
-  var cmin = Math.min(ratiodR,ratiodG,ratiodB),
-      cmax = Math.max(ratiodR,ratiodG,ratiodB),
-      delta = cmax - cmin,
-      h;
+  var cmin = Math.min(ratiodR, ratiodG, ratiodB),
+    cmax = Math.max(ratiodR, ratiodG, ratiodB),
+    delta = cmax - cmin,
+    h;
 
-  if(delta === 0) {
-      h = 0;
-  }
-  else if(cmax === ratiodR) {
-      h = ((ratiodG - ratiodB) / delta) % 6;
-  }
-  else if(cmax === ratiodG) {
-      h = (ratiodB - ratiodR) / delta + 2;
-  }
-  else {
-      h = (ratiodR - ratiodG) / delta + 4;
+  if (delta === 0) {
+    h = 0;
+  } else if (cmax === ratiodR) {
+    h = ((ratiodG - ratiodB) / delta) % 6;
+  } else if (cmax === ratiodG) {
+    h = (ratiodB - ratiodR) / delta + 2;
+  } else {
+    h = (ratiodR - ratiodG) / delta + 4;
   }
 
   h = Math.round(h * 60);
@@ -98,66 +115,67 @@ Pulsar.registerFunction("rgbaToHsla", function (r, g, b, a = 1) {
   var hue = h + (h < 0 ? 360 : 0);
 
   var light = (cmax + cmin) / 2;
-  var lightness = Math.round((((cmax + cmin) / 2) * 100));
-  var saturation = Math.round(((delta === 0 ? 0 : delta / (1 - Math.abs(2 * light - 1))) * 100));
+  var lightness = Math.round(((cmax + cmin) / 2) * 100);
+  var saturation = Math.round(
+    (delta === 0 ? 0 : delta / (1 - Math.abs(2 * light - 1))) * 100
+  );
 
   var alpha = Math.round((a / 255) * 10) / 10;
 
-  return "hsla(" + hue + "," + saturation + "%," + lightness + "%," + alpha + ")";
+  return (
+    "hsla(" + hue + "," + saturation + "%," + lightness + "%," + alpha + ")"
+  );
 });
 
 Pulsar.registerFunction("subFamilyToWeight", function (subfamily) {
   const cleanSubFamily = subfamily.toLowerCase;
   switch (cleanSubFamily) {
-    case "thin": 
+    case "thin":
       return 100;
-	  case "extralight": 
+    case "extralight":
       return 200;
-	  case "light": 
+    case "light":
       return 300;
-	  case "normal": 
+    case "normal":
       return 400;
-	  case "medium": 
+    case "medium":
       return 500;
-	  case "semibold": 
+    case "semibold":
       return 600;
-	  case "bold": 
+    case "bold":
       return 700;
-	  case "extrabold": 
+    case "extrabold":
       return 800;
-	  case "black": 
+    case "black":
       return 900;
-    default: 
+    default:
       return 400;
   }
 });
 
 Pulsar.registerFunction("pixelsToRem", function (value) {
-  return `${value['measure'] / 10}rem`;
+  return `${value["measure"] / 10}rem`;
 });
 
 Pulsar.registerFunction("logKeys", function (object) {
-  for(const entry in object) {
+  for (const entry in object) {
     console.log(entry);
   }
 });
 
-Pulsar.registerFunction(
-  "parseTokenType",
-  function (token) {
-    if(token.tokenType === "Text") {
-      const typeFromName = token.name.split('/')[0];
-      
-      return typeFromName;
-    }
+Pulsar.registerFunction("parseTokenType", function (token) {
+  if (token.tokenType === "Text") {
+    const typeFromName = token.name.split("/")[0];
 
-    return `${token.tokenType.toLowerCase()}`;
+    return typeFromName;
   }
-);
+
+  return `${token.tokenType.toLowerCase()}`;
+});
 
 Pulsar.registerFunction("baseWrap", function (token) {
-  const stringPrefix = token.split(':')[0];
-  if(stringPrefix === 'data') {
+  const stringPrefix = token.split(":")[0];
+  if (stringPrefix === "data") {
     return `url("${token}")`;
   }
 
